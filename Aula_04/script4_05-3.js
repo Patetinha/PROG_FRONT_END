c195f83076aaca9bead80480fd72997ebd28a48e
Rodrigo <diguinho.lokoo@gmail.com>
2022-10-06 19:38:17 -0300

    up

--- Aula_04/script4_05-3.js
+++ Aula_04/script4_05-3.js
@@ -1,11 +1,13 @@
-let i,j;
+let i,j,x;
 i=100;
 j=100;
-i++;
-i++;
-i++;
-i++;
-j--;
-j--;
-j--;
-document.write(`i=${i} j=${j}`);
+i++;//101
+i++;//102
+i++;//103
+i++;//104
+j--;//99
+j--;//98
+j--;//97
+x=i+j;//recebe o valor
+i+=j;//i recebe o valor da linha 11
+document.write(`i=${i} j=${j} x=${x}`);//i=104 j=97
