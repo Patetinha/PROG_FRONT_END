9d726fc912fd8e9903b57f7e8b96faec08b8a1e3
Rodrigo <diguinho.lokoo@gmail.com>
2022-11-22 20:54:56 -0300

    22/11/2022
    up

--- Aula_05/script5_09.js
+++ Aula_05/script5_09.js
@@ -1,7 +1,21 @@
-let i;
-const saida=document.querySelector(".saida");
-i=0;
-do{
-saida.innerHTML+=i + "<br>";
-i++;
-}while (i < 3);
+let i; //declaração de var 1
+const saida=document.querySelector(".saida"); //referencia de uma class="saida"
+i=10; //var i recebe o valor 0, valor inicial
+do{ // faz
+    saida.innerHTML+=i + "<br>"; //saida 
+    i++; //soma 1
+}while (i < 3); // condição 1 menor 3
+    saida.innerHTML+= "<hr>"; //saida
+    i=10;
+while(i<3){
+    saida.innerHTML+=i + "<br>"; //saida
+    i++;
+}
+
+/* 
+impressão
+
+0
+1
+2
+*/ 
